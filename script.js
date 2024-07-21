770d18cdbdc3d9627f3891d481fcb828c7dce966
Uday Bera <berauday86@gmail.com>
2024-07-22 01:49:44 +0530

    home section almost done

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function valueSetters() {
+    gsap.set(".home .parent .child", { y: "100%", rotateX: "135deg", opacity: 0 });
+    gsap.set(".home .arrow-down", { opacity: 0 })
+}
+
 function spanReveal() {
     document.querySelectorAll(".reveal")
         .forEach(function (elem) {
@@ -54,9 +59,61 @@ tl
         bottom: "100%",
         duration: 1,
         delay: -.1,
-        ease: Power4.inOut
+        ease: Power4.inOut,
+        onUpdate: function() {
+            var progress = tl.progress();
+
+            if (progress >= 0.8) {
+                homeAnimation();
+            }
+        },
+        onComplete: function() {
+            homeAnimation();
+        }
     },"b")
 }
 
+function homeAnimation() {
+    var tl = gsap.timeline();
+
+    tl
+    .to(".home-title .child", {
+        y: 0,
+        rotateX: 0,
+        opacity: 1,
+        stagger: .1,
+        duration: 1,
+    })
+    .to(".home-title-first-right .child", {
+        y: 0,
+        rotateX: 0,
+        opacity: 1,
+        stagger: .1,
+        duration: 1,
+    },"c")
+    .to(".home .arrow-down", {
+        opacity: 1,
+        duration: 1,
+        onComplete: function() {
+            animateSvg();
+        }
+    },"c")
+}
+
+function animateSvg() {
+    document.querySelectorAll("#web path").forEach(function(e) {
+        e.style.strokeDasharray = e.getTotalLength() + "px";
+        e.style.strokeDashoffset = e.getTotalLength() + "px";
+    })
+
+    gsap.to("#web>path, #web>mask>path", {
+        strokeDasharray: 0,
+        strokeDashoffset: 0,
+        duration: 2,
+    })
+}
+
+
 spanReveal();
+valueSetters();
 loadingAnimation();
